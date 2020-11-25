fa553b23d5f7dc6e9c781e368a97447071ac8efa
flaviodiminuto <flavio.diminuto@outlook.com>
2020-11-25 01:30:02 -0300

    [update] Adicao do modelo para Perfil Inicial

--- src/main/java/com/sexto/ia/model/PerfilInicial.java
+++ src/main/java/com/sexto/ia/model/PerfilInicial.java
@@ -1,2 +1,33 @@
-package com.sexto.ia.model;public class PerfilInicial {
+package com.sexto.ia.model;
+
+import java.util.List;
+
+public class PerfilInicial {
+    private List<Long> filmesMaisLegais;
+    private List<Long> filmesNormais;
+    private List<Long> filmesMenosLegais;
+
+    public List<Long> getFilmesMaisLegais() {
+        return filmesMaisLegais;
+    }
+
+    public void setFilmesMaisLegais(List<Long> filmesMaisLegais) {
+        this.filmesMaisLegais = filmesMaisLegais;
+    }
+
+    public List<Long> getFilmesNormais() {
+        return filmesNormais;
+    }
+
+    public void setFilmesNormais(List<Long> filmesNormais) {
+        this.filmesNormais = filmesNormais;
+    }
+
+    public List<Long> getFilmesMenosLegais() {
+        return filmesMenosLegais;
+    }
+
+    public void setFilmesMenosLegais(List<Long> filmesMenosLegais) {
+        this.filmesMenosLegais = filmesMenosLegais;
+    }
 }
